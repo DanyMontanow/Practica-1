4594ebddaccedc11f1cf465a0f834ee6f7d0c6a6
salma <danielamontano2102@gmail.com>
2016-09-07 19:17:04 -0700

    clasesnuevas

---
+++ Practica1/Practica1/Alumno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+   public class Alumno: Persona
+    {
+      public int NoControl;
+      public string Materias;
+       
+    }
+}
--- Practica1/Practica1/Program.cs
+++ Practica1/Practica1/Program.cs
@@ -10,6 +10,20 @@ namespace Practica1
     {
         static void Main(string[] args)
         {
+            Alumno _alumno = new Alumno();
+            Profesor _profesor = new Profesor();
+            _alumno.edad = 17;
+            _alumno.nombre = "daniela";
+            _alumno.NoControl = 0694;
+
+            _profesor.edad = 34;
+            _profesor.nombre = "Mene";
+            _profesor.Sueldo = 1600;
+
+            Console.WriteLine("La edad del alumno es: {0} su nombre es {1} y su no de Control es {2}", _alumno.edad, _alumno.nombre, _alumno.NoControl);
+            Console.WriteLine("La edad del maestro es: {0} su nombre es {1} y su sueldo es {2}", _profesor.edad, _profesor.nombre, _profesor.Sueldo);
+            Console.ReadKey();
         }
+
     }
 }
